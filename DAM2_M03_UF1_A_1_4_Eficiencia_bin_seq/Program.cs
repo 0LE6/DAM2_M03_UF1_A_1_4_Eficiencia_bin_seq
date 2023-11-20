79adc3e689b4abaecf6b978c674379b6f456dc2b
Oleg <kharenkoleg@gmail.com>
2023-11-20 19:50:26 +0100

    Agregar archivos de proyecto.

---
+++ DAM2_M03_UF1_A_1_4_Eficiencia_bin_seq/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        // Práctica A1.4
+
+
+        int[] array = { 5, 4, 6, 7, 8 };
+        int a = 1;
+        Console.WriteLine(a);
+
+        Array.Sort(array);
+
+    }
+}
