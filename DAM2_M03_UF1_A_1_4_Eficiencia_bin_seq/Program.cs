35ab7efa546efcaea8f3d05464dd8a50058a0f59
Oleg <kharenkoleg@gmail.com>
2023-11-20 20:38:15 +0100

    dfsd

--- DAM2_M03_UF1_A_1_4_Eficiencia_bin_seq/Program.cs
+++ DAM2_M03_UF1_A_1_4_Eficiencia_bin_seq/Program.cs
@@ -5,12 +5,48 @@
 
         // Práctica A1.4
 
+        // Creamos StreamWriter para generar csv
+        StreamWriter sW = new StreamWriter("resultats.csv");
 
-        int[] array = { 5, 4, 6, 7, 8 };
-        int a = 1;
-        Console.WriteLine(a);
+        /* resultats.csv
+        Mida;Cerca seqüencial;Cerca dicotòmica
+        500;???;??
+        1000;???;??
+        1500;???;??
+        2000;???;??
+        …
+        100000;???;??
+        */
 
-        Array.Sort(array);
+        for (int i = 500; i < 100000; i += 500)
+        {
+            int[] vector = new int[i];
+        }
 
+        // Búsqueda lineal
+        int[] arrayParaBusquedaSec = { 3, 5, 1, 2, 8 };
+        int targetSec = 8;
+        BusquedaSecuencial(arrayParaBusquedaSec, targetSec);
+
+        // Búsqueda dicotómica
+        int[] arrayParaBusquedaDic = { 3, 5, 1, 2, 8 };
+        int targetDic = 8;
+        BusquedaDicotomica(arrayParaBusquedaDic, targetDic);
+
+    }
+
+    public static int BusquedaSecuencial(int[] array, int target)
+    {
+        int low = 0;
+        int high = array.Length - 1;
+        int cont = 0;
+
+
+        return 0;
+    }
+
+    public static int BusquedaDicotomica(int[] array, int target)
+    {
+        return 0;
     }
 }
